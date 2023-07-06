0a49fda0850ea27ec0067be82e6543269aad6253
fplonka <github@1f389.com>
2023-07-06 08:55:45 -0400

    Adjust Typography <pre> styles

--- src/routes/post/[id]/+page.svelte
+++ src/routes/post/[id]/+page.svelte
@@ -60,7 +60,7 @@
 			</button>
 		</div>
 		<div
-			class="text-black mt-2 pb-4 prose-code:bg-red-500 prose prose-img:mx-auto prose-img:mb-1 prose-quoteless prose-blockquote:font-normal hover:prose-a:text-gray-500 relative"
+			class="prose text-black mt-2 pb-4 prose-pre:rounded-none prose-pre:bg-gray-100 prose-pre:text-black prose-img:mx-auto prose-img:mb-1 prose-quoteless prose-blockquote:font-normal hover:prose-a:text-gray-500 relative"
 		>
 			{@html data.post.body}
 		</div>
