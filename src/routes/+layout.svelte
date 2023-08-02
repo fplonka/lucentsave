e2858d5ddd928e65ae6d9769cc4cc98fa1c117fe
fplonka <github@1f389.com>
2023-08-02 08:58:11 -0400

    Fix canonical URL

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,9 +5,9 @@
 <MetaTags
 	title="Lucentsave"
 	description="A simple website for saving and organizing the things you read online."
-	canonical="https://lucentsave.com/"
+	canonical="https://lucentsave.com"
 	openGraph={{
-		url: 'https://lucentsave.com/',
+		url: 'https://lucentsave.com',
 		title: 'Lucentsave',
 		description: 'A simple website for saving and organizing the things you read online.',
 		site_name: 'Lucentsave'
